c0881577815661c8d139af9109fede7f463590ac
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-05-26 09:22:35 +0300

    homework 6 DSA

---
+++ Programming/5. DSA/06.DataStructuresEfficiency/02.TradeCompany/TradeCompany.cs
@@ -0,0 +1,35 @@
+﻿// 2. A large trade company has millions of articles, each described by barcode, vendor, title and price. 
+// Implement a data structure to store them that allows fast retrieval of all articles in given price range [x…y].
+// Hint: use OrderedMultiDictionary<K,T> from Wintellect's Power Collections for .NET. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace _02.TradeCompany
+{
+    class TradeCompany
+    {
+        static void Main(string[] args)
+        {
+            OrderedMultiDictionary<decimal, Article> store = new OrderedMultiDictionary<decimal, Article>(true);
+            store.Add(1.00m, new Article(1234567890, "Savona", "Liquid soap"));
+            store.Add(1.50m, new Article(1234567890, "Savona", "Sponge"));
+            store.Add(1.60m, new Article(1234567890, "Clarion", "Sponge"));
+            store.Add(1.07m, new Article(1234567890, "Clarion", "Magic Cream"));
+            store.Add(1.11m, new Article(1234567890, "Savona", "Shampoo"));
+            store.Add(1.23m, new Article(1234567890, "Clarion", "Shampoo"));
+            store.Add(1.40m, new Article(1234567890, "Savona", "Shower gel"));
+            store.Add(1.27m, new Article(1234567890, "Hunny", "Liquid soap"));
+            store.Add(1.33m, new Article(1234567890, "Hunny", "Shower gel"));
+            var filteredAtricles = store.Range(1.20m, true, 1.40m, true);
+            foreach (var article in filteredAtricles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+}
---
+++ Programming/5. DSA/06.DataStructuresEfficiency/03.MultiBiDictonary/MultiBiDictionaryTest.cs
@@ -0,0 +1,38 @@
+﻿// 3.Implement a class BiDictionary<K1,K2,T> that allows adding triples {key1, key2, value} and fast search
+// by key1, key2 or by both key1 and key2. Note: multiple values can be stored for given key.
+
+namespace _03.MultiBiDictonary
+{
+    using System;
+    using System.Linq;
+
+    public class MultiBiDictionaryTest
+    {
+        public static void Main()
+        {
+            var students = new MultiBiDictonary<string, string, int>(true);
+            students.Add("Ivan", "Ivanov", 3);
+            students.Add("Ivan", "Petrov", 2);
+            students.Add("Ivan", "Hristov", 3);
+            students.Add("Petar", "Ivanov", 4);
+            students.Add("Petar", "Hristov", 4);
+            students.Add("Petar", "Tzolov", 5);
+            students.Add("Hristo", "Ivanov", 5);
+            students.Add("Hristo", "Uzunov", 3);
+            students.Add("Petar", "Ivanov", 6);
+
+            var searchName = "Ivanov";
+            Console.WriteLine("Average grade of {0}:", searchName);
+            Console.WriteLine(students.FindSecondKey(searchName).Sum() * 1.0 / students.FindSecondKey(searchName).Count);
+
+            searchName = "Petar";
+            Console.WriteLine("Average grade of {0}:", searchName);
+            Console.WriteLine(students.FindFirstKey(searchName).Sum() * 1.0 / students.FindFirstKey(searchName).Count);
+
+            searchName = "Petar";
+            var searchFamilyName = "Ivanov";
+            Console.WriteLine("Average grade of {0} {1}:", searchName, searchFamilyName);
+            Console.WriteLine(students.FindBothKeys(searchName, searchFamilyName).Sum() * 1.0 / students.FindBothKeys(searchName, searchFamilyName).Count);
+        }
+    }
+}
