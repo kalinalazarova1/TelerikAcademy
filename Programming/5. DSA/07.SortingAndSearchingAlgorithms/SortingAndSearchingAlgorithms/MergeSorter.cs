0046cb9347b5cd504f37ea70b82bec88aba62ab6
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-05-30 16:59:06 +0300

    homework 7

---
+++ Programming/5. DSA/07.SortingAndSearchingAlgorithms/SortingAndSearchingAlgorithms/MergeSorter.cs
@@ -0,0 +1,57 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MergeSorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            var mergeCollection = new T[collection.Count];
+            this.MergeSort(collection, mergeCollection, 0, collection.Count - 1);
+        }
+
+        private void MergeSort(IList<T> collection, IList<T> mergeCollection, int l, int r)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+
+            int m = l + ((r - l) / 2);
+            this.MergeSort(collection, mergeCollection, l, m);
+            this.MergeSort(collection, mergeCollection, m + 1, r);
+            this.Merge(collection, mergeCollection, l, m, r);
+        }
+
+        private void Merge(IList<T> collection, IList<T> mergeCollection, int l, int m, int r)
+        {
+            for (int i = l, left = l, right = m + 1; left <= m || right <= r; i++)
+            {
+                if (left > m)
+                {
+                    mergeCollection[i] = collection[right++];
+                }
+                else if (right > r)
+                {
+                    mergeCollection[i] = collection[left++];
+                }
+                else if (collection[left].CompareTo(collection[right]) <= 0)
+                {
+                    mergeCollection[i] = collection[left++];
+                }
+                else if (collection[left].CompareTo(collection[right]) > 0)
+                {
+                    mergeCollection[i] = collection[right++];
+                }
+            }
+
+            for (int i = l; i <= r; i++)
+            {
+                collection[i] = mergeCollection[i];
+            }
+
+            return;
+        }
+    }
+}
---
+++ Programming/5. DSA/07.SortingAndSearchingAlgorithms/SortingAndSearchingAlgorithms/SelectionSorter.cs
@@ -0,0 +1,36 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SelectionSorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            collection = this.SelectionSort(collection);
+        }
+
+        private IList<T> SelectionSort(IList<T> collection)
+        {
+            int min;
+            T forSwap;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                min = i;
+                for (int j = i; j < collection.Count; j++)
+                {
+                    if (collection[j].CompareTo(collection[min]) < 0)
+                    {
+                        min = j;
+                    }
+                }
+
+                forSwap = collection[i];
+                collection[i] = collection[min];
+                collection[min] = forSwap;
+            }
+
+            return collection;
+        }
+    }
+}
