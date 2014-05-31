66aa2b1bdcf7c1aa66f2284eda4dab5d8bb8ce4e
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-05-31 16:15:35 +0300

    homework 8 tasks 1 to 11

---
+++ Programming/5. DSA/08.Recursion/01.NestedLoops/NestedLoops.cs
@@ -0,0 +1,30 @@
+﻿// 1.Write a recursive program that simulates the execution of n nested loops from 1 to n.
+
+namespace _01.NestedLoops
+{
+    using System;
+
+    internal class NestedLoops
+    {
+        internal static void Main()
+        {
+            int n = 3;
+            GetLoops(0, 1, n, new int[n]);
+        }
+
+        private static void GetLoops(int startIndex, int startValue, int endValue, int[] loops)
+        {
+            if (startIndex == loops.Length)
+            {
+                Console.WriteLine(string.Join(" ", loops));
+                return;
+            }
+
+            for (int i = startValue; i <= endValue; i++)
+            {
+                loops[startIndex] = i;
+                GetLoops(startIndex + 1, startValue, endValue, loops);
+            }
+        }
+    }
+}
---
+++ Programming/5. DSA/08.Recursion/02.CombinationWithDuplicates/CombinationWithDuplicates.cs
@@ -0,0 +1,33 @@
+﻿// 2.Write a recursive program for generating and printing all the combinations with duplicates of k elements
+// from n-element set. Example:
+// n=3, k=2 -> (1 1), (1 2), (1 3), (2 2), (2 3), (3 3)
+
+namespace _02.CombinationWithDuplicates
+{
+    using System;
+
+    internal class CombinationWithDuplicates
+    {
+        internal static void Main()
+        {
+            int n = 3;
+            int k = 2;
+            GetCombDuplicates(0, 1, n, new int[k]);
+        }
+
+        private static void GetCombDuplicates(int startIndex, int startValue, int endValue, int[] comb)
+        {
+            if (startIndex == comb.Length)
+            {
+                Console.Write("({0}), ", string.Join(" ", comb));
+                return;
+            }
+
+            for (int i = startValue; i <= endValue; i++)
+            {
+                comb[startIndex] = i;
+                GetCombDuplicates(startIndex + 1, i, endValue, comb);
+            }
+        }
+    }
+}
