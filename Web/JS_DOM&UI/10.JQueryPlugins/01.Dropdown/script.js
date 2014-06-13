0715dc2791bffcacef69b4af19ea9a572dc35921
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-06-13 21:18:42 +0300

    JS DOM&UI homework 9

---
+++ Web/JS_DOM&UI/10.JQueryPlugins/01.Dropdown/script.js
@@ -0,0 +1,61 @@
+﻿// 1.Create a jQuery plugin for creating dropdown list
+// By given the following:
+// <select id="dropdown">
+// <option value="1">One</option>
+// <option value="2">Two</option>
+// </select>
+// $('#dropdown').dropdown()
+// Produces the following HTML:
+// <select id="dropdown" style="display: none">…</select>
+// <div class="dropdown-list-container">
+//   <ul class="dropdown-list-options">
+//     <li class="dropdown-list-option" data-value="0">One</li>
+//     …
+//   </ul>
+// </div>
+// And make it work as SELECT node
+// Selecting an one of the generated LI nodes, selects the corresponding OPTION node
+// So $('#dropdown:selected') works
+
+/// <reference path="jquery-1.11.1.js" />
+
+(function ($) {
+    $.fn.dropdown = function () {
+        var $this,
+            $options,
+            $dropdown,
+            $current,
+            i;
+
+        function onClick() {
+            var childIndex = ($(this).attr('data-value') | 0) + 1;
+            $('#dropdown')
+                .find('option')
+                .removeAttr('selected');
+            $('#dropdown :nth-child(' + childIndex + ')')
+                .attr('selected', 'selected');
+            // click on item in the list to see the result in the console
+            console.log($('#dropdown option:selected').text());
+        }
+
+        $('#dropdown').css('display', 'none');
+        $this = $(this);
+        $options = $('<ul>')
+                .addClass('dropdown-list-options');
+        $dropdown = $('<div>')
+            .addClass('dropdown-list-container')
+            .append($options);
+        $current = $this.children().first();
+        for (i = 0; i < $this.children().length; i++) {
+            $('<li>')
+                .addClass('dropdown-list-option')
+                .text($current.text())
+                .attr('data-value', i)
+                .appendTo($options);
+            $current = $current.next();
+        }
+        $options.on('click', 'li', onClick);
+        $dropdown.appendTo($('body'));
+        return this;
+    };
+}(jQuery));
---
+++ Web/JS_DOM&UI/10.JQueryPlugins/02.MessageBox/script.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="jquery-1.11.1.js" />
+
+(function ($) {
+    $.fn.messageBox = function () {
+        var $this = $(this);
+
+        return {
+            success: function (message) {
+                $('#message-box')
+                    .text(message)
+                    .css('color', 'green')
+                    .fadeIn(1000)
+                    .delay(3000)
+                    .fadeOut(1000);
+                return $this;
+            },
+            error: function (message) {
+                $('#message-box')
+                    .text(message)
+                    .css('color', 'red')
+                    .fadeIn(1000)
+                    .delay(3000)
+                    .fadeOut(1000);
+                return $this;
+            }
+        };
+    };
+}(jQuery));
