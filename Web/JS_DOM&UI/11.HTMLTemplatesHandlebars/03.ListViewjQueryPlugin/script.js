37dadd1ac4c18e0e18127813032472afc5de576e
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-06-14 19:03:06 +0300

    JS DOM&UI homework 11 tasks 3 and 4

---
+++ Web/JS_DOM&UI/11.HTMLTemplatesHandlebars/03.ListViewjQueryPlugin/script.js
@@ -0,0 +1,19 @@
+﻿// 3.*Create a jQuery plugin for ListView
+// Apply a template for each item of a collection
+// Using the data-template attribute set the ID of the template to use for the items
+// Must work with different collections and templates
+
+/// <reference path="jquery-1.11.1.js" />
+/// <reference path="handlebars-v1.3.0.js" />
+
+(function ($) {
+    $.fn.listview = function (collection) {
+        var $this = $(this);
+        var templateId = $this.attr('data-template');
+        var template = Handlebars.compile($('#' + templateId).html());
+        for (var i = 0; i < collection.length; i++) {
+            $this.html($this.html() + template(collection[i]));
+        }
+        return this;
+    };
+}(jQuery));
---
+++ Web/JS_DOM&UI/11.HTMLTemplatesHandlebars/04.ExtendedLIstViewjQueryPlugin/script.js
@@ -0,0 +1,16 @@
+﻿// 4.*Extend the previous task to set the template inside the DOM element, instead of setting it with data-template
+
+/// <reference path="jquery-1.11.1.js" />
+/// <reference path="handlebars-v1.3.0.js" />
+
+(function ($) {
+    $.fn.listview = function (collection) {
+        var $this = $(this);
+        var template = Handlebars.compile($this.html());
+        $this.html(template(collection[0]));
+        for (var i = 1; i < collection.length; i++) {
+            $this.html($this.html() + template(collection[i]));
+        }
+        return this;
+    };
+}(jQuery));
