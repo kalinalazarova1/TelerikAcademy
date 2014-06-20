f716f6d7205c50560f97e904433bb6aa498a84de
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-06-21 00:48:02 +0300

    DSA homework 9

---
+++ Programming/5. DSA/09.Combinatorics/01.BinaryPasswords/BinaryPasswords.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.BinaryPasswords
+{
+    using System;
+    using System.Numerics;
+
+    internal class BinaryPasswords
+    {
+        internal static void Main()
+        {
+            BigInteger result = 1;
+            var input = Console.ReadLine().ToCharArray();
+            Array.Sort(input);
+            for (int i = 0; i < Array.LastIndexOf(input, '*') + 1; i++)
+            {
+                result *= 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming/5. DSA/09.Combinatorics/02.Circles/Circles.cs
@@ -0,0 +1,71 @@
+﻿// 90/100 BgCoder
+namespace _07.Circles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Circles
+    {
+        private static HashSet<string> necklaces = new HashSet<string>();
+        private static int counter = 0;
+        private static char[] comb;
+
+        internal static void Main()
+        {
+            var set = Console.ReadLine().ToCharArray();
+            var keys = set.GroupBy(x => x).Select(g => g.Key).ToArray();
+            var values = set.GroupBy(x => x).Select(g => g.Count()).ToArray();
+
+            if (set.Length == keys.Length && set.Length > 2)
+            {
+                var fact = 1;
+                for (int i = 0; i < set.Length; i++)
+                {
+                    fact *= i + 1;
+                }
+
+                Console.WriteLine(fact / (set.Length * 2));
+                return;
+            }
+
+            comb = new char[set.Length];
+            GetPerm(values, keys, 0);
+            Console.WriteLine(counter);
+        }
+
+        private static void GetPerm(int[] values, char[] keys, int index)
+        {
+            if (index == comb.Length)
+            {
+                if (!(necklaces.Contains(string.Join(string.Empty, comb)) || necklaces.Contains(string.Join(string.Empty, comb.Reverse<char>()))))
+                {
+                    // Console.WriteLine("{{{0}}}", string.Join(", ", comb));
+                    necklaces.Add(string.Join(string.Empty, comb));
+                    counter++;
+                    var cur = comb.ToList();
+                    for (int i = 0; i < comb.Length; i++)
+                    {
+                        char temp = cur[0];
+                        cur.RemoveAt(0);
+                        cur.Add(temp);
+                        necklaces.Add(string.Join(string.Empty, cur));
+                    }
+
+                    return;
+                }
+            }
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (values[i] > 0)
+                {
+                    comb[index] = keys[i];
+                    values[i]--;
+                    GetPerm(values, keys, index + 1);
+                    values[i]++;
+                }
+            }
+        }
+    }
+}
