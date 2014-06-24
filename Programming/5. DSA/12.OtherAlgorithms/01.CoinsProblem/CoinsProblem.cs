b7e2e6f8824bdd2abf795c6c46195629eaa01fbf
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-06-24 15:14:00 +0300

    DSA homework 12

---
+++ Programming/5. DSA/12.OtherAlgorithms/01.CoinsProblem/CoinsProblem.cs
@@ -0,0 +1,27 @@
+﻿// 1. You are given a set of infinite number of coins (1, 2 and 5) and end value – N. Write an algorithm
+// that gives the number of coins needed so that the sum of the coins equals N.
+// Example: N = 33 => 6 coins x 5 + 1 coin x 2 + 1 coin x 1
+namespace _01.CoinsProblem
+{
+    using System;
+    using System.Linq;
+
+    internal class CoinsProblem
+    {
+        internal static void Main()
+        {
+            int[] coinValues = { 5, 2, 1 };
+            int[] coinsCount = new int[coinValues.Length];
+            int sum = 33;
+            int rest = sum;
+            for (int i = 0; i < coinValues.Length && rest > 0; i++)
+            {
+                coinsCount[i] = rest / coinValues[i];
+                Console.WriteLine("{0} coin/coins of value {1}", coinsCount[i], coinValues[i]);
+                rest -= coinValues[i] * coinsCount[i];
+            }
+
+            Console.WriteLine("We need total {0} coins to make the sum {1}.", coinsCount.Sum(), sum);
+        }
+    }
+}
---
+++ Programming/5. DSA/12.OtherAlgorithms/02.PointInTriangle/PointInTriangle.cs
@@ -0,0 +1,38 @@
+﻿// 2. You are given 3 points A, B and C, forming triangle, and a point P. Check if the point P is in the triangle or not.
+namespace _02.PointInTriangle
+{
+    using System;
+
+    internal class PointInTriangle
+    {
+        internal static void Main()
+        {
+            var p = new Point(1, 0.9);
+            var a = new Point(2, 0);
+            var b = new Point(0, 1);
+            var c = new Point(1, 1);
+            Console.WriteLine("The point P({0}, {1}) is in the triangle: {2}", p.X, p.Y, IsPointInTriangle(p, a, b, c));
+        }
+
+        private static bool IsPointInTriangle(Point p, Point a, Point b, Point c)
+        {
+            var s = (a.Y * c.X) - (a.X * c.Y) + ((c.Y - a.Y) * p.X) + ((a.X - c.X) * p.Y);
+            var t = (a.X * b.Y) - (a.Y * b.X) + ((a.Y - b.Y) * p.X) + ((b.X - a.X) * p.Y);
+
+            if ((s < 0) != (t < 0))
+            {
+                return false;
+            }
+
+            var area = -(b.Y * c.X) + (a.Y * (c.X - b.X)) + (a.X * (b.Y - c.Y)) + (b.X * c.Y);
+            if (area < 0.0)
+            {
+                s = -s;
+                t = -t;
+                area = -area;
+            }
+
+            return s > 0 && t > 0 && (s + t) < area;
+        }
+    }
+}
