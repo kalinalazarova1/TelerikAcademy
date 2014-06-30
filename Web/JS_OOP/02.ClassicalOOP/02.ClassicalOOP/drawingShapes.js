e53a6bf9cf174c1cdb8444e9788e5e80569d00c4
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-06-30 10:06:52 +0300

    added constructors for shapes

--- Web/JS_OOP/02.ClassicalOOP/02.ClassicalOOP/drawingShapes.js
+++ Web/JS_OOP/02.ClassicalOOP/02.ClassicalOOP/drawingShapes.js
@@ -1,7 +1,14 @@
-﻿var CanvasDrawer = (function () {
-    'use strict';
+﻿// 1.Create a module for drawing shapes using Canvas. Implement the following shapes:
+// Rect, by given position (X, Y) and size (Width, Height)
+// Circle, by given center position (X, Y) and radius (R)
+// Line, by given from (X1, Y1) and to (X2, Y2) positions
+
+var CanvasDrawer = (function () {
     var ctx;
     function CanvasDrawer(selector, args) {
+        if (!(this instanceof arguments.callee)) {
+            return new CanvasDrawer(selector, args);
+        }
         this.stroke = args.stroke || 'black';
         this.line = args.line || 1;
         this.fill = args.fill || 'white';
@@ -17,34 +24,68 @@
         ctx = canvas.getContext('2d');
     }
 
-    CanvasDrawer.prototype.drawLine = function (startX, startY, endX, endY) {
+    CanvasDrawer.prototype.drawLine = function (line) {
         ctx.beginPath();
-        ctx.moveTo(startX, startY);
-        ctx.lineTo(endX, endY);
+        ctx.moveTo(line.startX, line.startY);
+        ctx.lineTo(line.endX, line.endY);
         ctx.closePath();
         ctx.lineWidth = this.line;
         ctx.strokeStyle = this.stroke;
         ctx.stroke();
     };
 
-    CanvasDrawer.prototype.drawCircle = function (x, y, radius) {
+    CanvasDrawer.prototype.drawCircle = function (circle) {
         ctx.beginPath();
         ctx.fillStyle = this.fill;
         ctx.strokeStyle = this.stroke;
         ctx.lineWidth = this.line;
-        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
     };
 
-    CanvasDrawer.prototype.drawRect = function (x, y, width, height) {
+    CanvasDrawer.prototype.drawRect = function (rect) {
         ctx.fillStyle = this.fill;
         ctx.lineWidth = this.line;
         ctx.strokeStyle = this.stroke;
-        ctx.fillRect(x, y, width, height);
-        ctx.strokeRect(x, y, width, height);
+        ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
     };
 
     return CanvasDrawer;
+}());
+
+var shapes = (function () {
+    function Rect(x, y, width, height) {
+        if (!(this instanceof arguments.callee)) {
+            return new Rect(x, y, width, height);
+        }
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    function Circle(x, y, radius) {
+        if (!(this instanceof arguments.callee)) {
+            return new Circle(x, y, radius);
+        }
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    function Line(startX, startY, endX, endY) {
+        if (!(this instanceof arguments.callee)) {
+            return new Line(startX, startY, endX, endY);
+        }
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+    return {
+        Rect: Rect,
+        Circle: Circle,
+        Line: Line
+    };
 }());
