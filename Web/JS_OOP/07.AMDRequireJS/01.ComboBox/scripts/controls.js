6ff4c50cf526843c5aaa8b9c5ecedb1f29fb1a8a
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-07-06 18:06:55 +0300

    fix controls.js

--- Web/JS_OOP/07.AMDRequireJS/01.ComboBox/scripts/controls.js
+++ Web/JS_OOP/07.AMDRequireJS/01.ComboBox/scripts/controls.js
@@ -12,9 +12,8 @@ define(['combo-box-item', 'libs/jquery'], function (ComboBoxItem) {
         ComboBox.prototype.render = function (htmlTemplate) {
             var self = this;
             var $combo = $('<div>')
-                .addClass('combo-box')
-            $('body').on('click', '.person-item', function () {
-                var $this = $(this);
+            $('body').on('click', '#combo-box .person-item', function () {
+            var $this = $(this);
                 if (!self._collapsed) {
                     self._collapsed = true;
                     $this.parent().find('.person-item').hide();
@@ -24,14 +23,14 @@ define(['combo-box-item', 'libs/jquery'], function (ComboBoxItem) {
                     self._collapsed = false;
                 }
             });
-            $('<div>')
-                .addClass('list')
-                .appendTo($combo);
+            
             for (var i = 0; i < this._items.length; i++) {
                 $combo.html($combo.html() + ComboBoxItem(this._items[i]).render(htmlTemplate));
                 $combo.find('.person-item').hide().first().show();
             }
-            return $combo.html();
+            var $list = $('<div>').append($('<div id="combo-box">').append($combo));
+
+            return $list.html();
         };
 
         return {
