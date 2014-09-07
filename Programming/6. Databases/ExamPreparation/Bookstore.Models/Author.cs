e3d50f4f8e64823c0c89b9d46fd45aa1eab60e8c
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-09-08 00:12:02 +0300

    small fix

--- Programming/6. Databases/ExamPreparation/Bookstore.Models/Author.cs
+++ Programming/6. Databases/ExamPreparation/Bookstore.Models/Author.cs
@@ -1,15 +1,31 @@
 ﻿namespace Bookstore.Models
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class Author
     {
+        private ICollection<Book> books;
+
+        public Author()
+        {
+            this.books = new HashSet<Book>();
+        }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public virtual ICollection<Book> Books 
+        {
+            get
+            {
+                return this.books;
+            }
+
+            set
+            {
+                this.books = value;
+            }
+        }
     }
 }
